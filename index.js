1a9e3d5931fb4420ed507ca6a6e117d1c3b8d7d1
niko wahyu fitrianto <nikonabilah@gmail.com>
2023-02-01 17:39:30 +0700

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+var hehe = [1, 2, 3, 4, 6, 5, 7, 8, 9, 10, 11, 13, 12, 14, 15, 16, 17, 18, 19, 20];
+var kiri = 1;
+for(let i = 0 ; i < hehe.length; i++)
+{
+  if(i < ((hehe.length / 2)/2)){
+		console.log(hehe[i]+ "->" + hehe[((hehe.length / 2) - 1) - i] + "->" + hehe[i])
+    if(i == (((hehe.length / 2)/2) -1))
+    {
+      i = hehe.length / 2;
+    }
+	}
+
+  if(i > ((hehe.length / 2)/2)){
+   console.log(hehe[i] + "->" + hehe[(hehe.length) - kiri] + "->"+hehe[(hehe.length / 2) - kiri])
+   kiri++;
+  }
+}
